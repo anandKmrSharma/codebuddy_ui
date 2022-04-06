3c084c6deb36f544a8700903b808aea9c42c4806
anandKmrSharma <87422058+anandKmrSharma@users.noreply.github.com>
2022-04-06 19:47:35 +0530

    y

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="app1">
+
+    <div className='header'>
+<div className='left'>
+      <img className="image" src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Microsoft_Bing_logo.svg/2560px-Microsoft_Bing_logo.svg.png'/>
+
+  
+<div className='header'>image videos translate</div>
+
+      </div>
+{/* <div className='right'><p>sign in</p></div> */}
+
+    </div> 
+
+    {/* <div className='right'><p>sign in</p></div> */}
+
+<div className='inputbox'><input className='input' type="text" placeholder="search"/></div>
+
+</div>
+  );
+}
+
+export default App;
+
+
